e5315ca2d71d292ba034eff90971582ccfd3b3a1
Shelby Sanchez <shelby@bluearrowmail.com>
2026-04-11 19:36:57 -0400

    completed implement trie

---
+++ ggp-problems/medium_implement_trie.py
@@ -0,0 +1,35 @@
+class Trie:
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for i in word:
+            if i not in curr:
+                curr[i] = {}
+            curr = curr[i]
+        curr["*"] = ""
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for i in word:
+            if i not in curr:
+                return False
+            curr = curr[i]
+        return "*" in curr
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for i in prefix:
+            if i not in curr:
+                return False
+            curr = curr[i]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
