6abf3fd0a79b11f2eca493f84470816fd5433206
Shelby Sanchez <shelby@bluearrowmail.com>
2026-04-22 20:49:23 -0400

    completed longest substring with at most k distinct characters

---
+++ ggp-problems/medium_longest_substring_with_at_most_k_distinct_characters.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        l, r, max_len = 0,0,0
+        map = defaultdict(int)
+
+        def handleOldestIndex():
+            """
+            removes smallest index from map
+            returns the index l should be at
+            """
+            smallest = float('inf')
+            smallest_i = None
+            for i in map:
+                if map[i] < smallest:
+                    smallest = map[i]
+                    smallest_i = i
+            del map[smallest_i]
+            return smallest + 1
+
+        while r < len(s):
+            map[s[r]] = r
+            if len(map) == k + 1:
+                l = handleOldestIndex()
+            max_len = max(max_len, r - l + 1)
+            r += 1
+
+        return max_len
