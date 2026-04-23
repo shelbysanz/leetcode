3e765b4f6631546888dc19b4b64697d5f8dde15f
Shelby Sanchez <shelby@bluearrowmail.com>
2026-04-22 20:33:45 -0400

    improved longest substring with at most two distinct characters

--- ggp-problems/medium_longest_substring_with_at_most_two_distinct_characters.py
+++ ggp-problems/medium_longest_substring_with_at_most_two_distinct_characters.py
@@ -2,25 +2,28 @@ from collections import defaultdict
 
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-        dist = ''
-        curr = ''
-        counts = defaultdict(int)
-        n_counts = defaultdict(int)
-        max_len = float('-inf')
+        l, r, max_len = 0,0,0
+        map = defaultdict(int)
 
-        for i in s:
-            if i == dist or i == curr:
-                if i != curr:
-                    dist, curr = curr, dist
-                    n_counts[curr] = 0
-            else:
-                dist = curr
-                curr = i
-                counts[curr] = 0
-                counts[dist] = n_counts[dist]
-            n_counts[dist] = 0
-            counts[curr] += 1
-            n_counts[curr] += 1
-            max_len = max(max_len, counts[dist] + counts[curr])
+        def handleOldestIndex():
+            """
+            removes smallest index from map
+            returns the index l should be at
+            """
+            smallest = float('inf')
+            smallest_i = None
+            for i in map:
+                if map[i] < smallest:
+                    smallest = map[i]
+                    smallest_i = i
+            del map[smallest_i]
+            return smallest + 1
+
+        while r < len(s):
+            map[s[r]] = r
+            if len(map) == 3:
+                l = handleOldestIndex()
+            max_len = max(max_len, r - l + 1)
+            r += 1
 
         return max_len
