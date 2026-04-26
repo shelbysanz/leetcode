211e95213475f6fa70f5f974897156bea6385c5d
shelbysanz <shelbsgt500@gmail.com>
2026-04-26 19:02:51 -0400

    improved solution expressive words - optimal time complexity

--- ggp-problems/medium_expressive_words.py
+++ ggp-problems/medium_expressive_words.py
@@ -1,44 +1,37 @@
 class Solution:
-    def expressiveWords(self, s: str, words: List[str]) -> int:
-        if not s or not words:
-            return 0
-        s += "*"
+    def getParts(self, str_val):
+        str_val += "*"
         parts = []
-        c_val = s[0]
-        c_index = 0
-        c_count = 1
-        for i in range(1, len(s)):
-            if c_val != s[i]:
-                if c_count >= 3:
-                    parts.append(f'*{c_val}')
-                else:
-                    parts.append(s[c_index:i])
+        c_count, c_val = 1, str_val[0]
+        for i in range(1, len(str_val)):
+            if c_val != str_val[i]:
+                parts.append((c_val, c_count))
                 c_count = 1
-                c_val = s[i]
-                c_index = i
+                c_val = str_val[i]
             else:
                 c_count += 1
-        
-        prefix = set()
-        for i in parts:
-            if "*" in i:
-                if not prefix:
-                    prefix.add(f"{i[1]}")
-                    prefix.add(f"{i[1]}{i[1]}")
-                else:
-                    new_prefix = set()
-                    for j in prefix:
-                        new_prefix.add(f"{j}{i[1]}")
-                        new_prefix.add(f"{j}{i[1]}{i[1]}")
-                    prefix = new_prefix
-            else:
-                if not prefix:
-                    prefix.add(i)
-                else:
-                    new_prefix = set()
-                    for j in prefix:
-                        new_prefix.add(f"{j}{i}")
-                    prefix = new_prefix
-        return len(prefix & set(words))
-                
-            
+        return parts
+
+    def expressiveWords(self, s: str, words: list[str]) -> int:
+        if not s or not words:
+            return 0
+
+        parts = self.getParts(s)
+        matches = 0
+        for word in words:
+            word = self.getParts(word)
+            if len(word) != len(parts):
+                continue
+            isMatch = True
+            for i in range(len(word)):
+                w_char, p_char = word[i][0], parts[i][0]
+                w_count, p_count = word[i][1], parts[i][1]
+
+                if w_char != p_char:
+                    isMatch = False
+                    break
+                if p_count < w_count or (p_count < 3 and p_count != w_count):
+                    isMatch = False
+                    break
+            matches += 1 if isMatch else 0
+        return matches
