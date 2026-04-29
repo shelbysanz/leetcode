a2453d25c674c68a53de6ee17afcb50ff9cb69c1
Shelby Sanchez <shelbsgt500@gmail.com>
2026-04-28 20:59:02 -0400

    improved trapping rain water

--- ggp-problems/hard_trapping_rain_water.py
+++ ggp-problems/hard_trapping_rain_water.py
@@ -2,20 +2,22 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         rain_count = 0
         n = len(height)
+
+        def getBounds():
+            bounds = [[0, 0] for _ in range(n)]
+            l_max, r_max = 0, 0
+            for i in range(n):
+                l_max = max(l_max, height[i])
+                bounds[i][0] = l_max
+            for i in range(n - 1, -1, -1):
+                r_max = max(r_max, height[i])
+                bounds[i][1] = r_max
+            return bounds
+
+        bounds = getBounds()
         for i in range(1, n - 1):
             curr_height = height[i]
-            l, r = i - 1, i + 1
-            l_bound, r_bound = height[l], height[r]
-            while l >= 0:
-                l_bnd = height[l]
-                if l_bnd > l_bound:
-                    l_bound = l_bnd
-                l -= 1
-            while r < n:
-                r_bnd = height[r]
-                if r_bnd > r_bound:
-                    r_bound = r_bnd
-                r += 1
+            l_bound, r_bound = bounds[i]
             bound = min(l_bound, r_bound)
             rain_count += max(0, bound - curr_height)
         return rain_count
