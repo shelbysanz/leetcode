4d9615086fde862a5a56e1ab9bfd91bc155db07d
Shelby Sanchez <shelbsgt500@gmail.com>
2026-04-28 21:47:00 -0400

    completed kth largest element in an array - iterative implementation of quickselect

--- ggp-problems/kth_largest_element_in_an_array.py
+++ ggp-problems/kth_largest_element_in_an_array.py
@@ -1,29 +1,34 @@
 import random
 
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         k = len(nums) - k
 
         def quickselect(l, r):
-            pivot = nums[random.randint(l, r)]
+            while True:
+                if r == l:
+                    return nums[l]
 
-            l_tmp, r_tmp = l, r
-            while l_tmp <= r_tmp:
-                while nums[l_tmp] < pivot:
-                    l_tmp += 1
-                while nums[r_tmp] > pivot:
-                    r_tmp -= 1
+                pivot = nums[random.randint(l, r)]
 
-                if l_tmp <= r_tmp:
-                    nums[l_tmp], nums[r_tmp] = nums[r_tmp], nums[l_tmp]
-                    l_tmp += 1
-                    r_tmp -= 1
+                l_tmp, r_tmp = l, r
+                while l_tmp <= r_tmp:
+                    while nums[l_tmp] < pivot:
+                        l_tmp += 1
+                    while nums[r_tmp] > pivot:
+                        r_tmp -= 1
 
-            if k >= l_tmp:
-                return quickselect(l_tmp, r)
-            elif k <= r_tmp:
-                return quickselect(l, r_tmp)
-            else:
-                return nums[k]      
+                    if l_tmp <= r_tmp:
+                        nums[l_tmp], nums[r_tmp] = nums[r_tmp], nums[l_tmp]
+                        l_tmp += 1
+                        r_tmp -= 1
+
+                if k >= l_tmp:
+                    l = l_tmp
+                elif k <= r_tmp:
+                    r = r_tmp
+                else:
+                    return nums[k]
 
         return quickselect(0, len(nums) - 1)
