90d807f0df3fce21ffd92650db4c15e817eb0f6f
shelbysanz <shelbsgt500@gmail.com>
2026-05-06 20:16:40 -0400

    improved insert interval (readability)

--- ggp-problems/medium_insert_interval.py
+++ ggp-problems/medium_insert_interval.py
@@ -2,23 +2,27 @@ class Solution:
     def insert(
         self, intervals: List[List[int]], newInterval: List[int]
     ) -> List[List[int]]:
-        l, r = 0, len(intervals)
-        while l < r:
-            m = l + (r - l) // 2
-            if intervals[m][1] < newInterval[0]:
-                l = m + 1
-            else:
-                r = m
-        start = l
-        output = intervals[:start]
+        output = []
+        i, n = 0, len(intervals)
 
-        i = start
-        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
+        new_start, new_end = newInterval
+
+        # left
+        while i < n and intervals[i][1] < new_start:
+            output.append(intervals[i])
             i += 1
 
-        output.append(newInterval)
-        output.extend(intervals[i:])
+        # merge
+        while i < n and intervals[i][0] <= new_end:
+            new_start = min(new_start, intervals[i][0])
+            new_end = max(new_end, intervals[i][1])
+            i += 1
+
+        output.append([new_start, new_end])
+
+        # right
+        while i < n:
+            output.append(intervals[i])
+            i += 1
 
         return output
